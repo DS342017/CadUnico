7b0584fe3179615459c52c7405904d919d5d2abe
Elizeu Elieber Fachini <elizeuelieber@live.com>
2018-11-13 06:47:19 -0200

    Adicionar arquivos de projeto.

---
+++ CadUnico/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CadUnico.Startup))]
+namespace CadUnico
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
